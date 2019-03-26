a59202e8ce7fefd354dc8db03dbfb85f70e672d2
Michelle G <gerbermichelle23@gmail.com>
2019-03-26 13:25:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,4 +8,12 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value;
   return object;
 }
-
+var newObj = Object.assign({}, obj);
+function deleteFromObjectByKey(object, key){
+  delete newObj[key];
+  return newObj;
+}
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key];
+  return object;
+}
