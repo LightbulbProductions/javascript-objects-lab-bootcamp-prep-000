5e37c539f674e338bd26328b790d9c82df090e44
Michelle G <gerbermichelle23@gmail.com>
2019-03-26 13:23:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ var obj = { prop: 1 };
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, obj, {[key]: value});
 }
-
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  
