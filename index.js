523ee9c66b55410ed0c09b1075d29942960991a4
Michelle G <gerbermichelle23@gmail.com>
2019-03-26 13:23:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, obj, {[key]: value});
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  
+  object[key] = value;
+}
+
